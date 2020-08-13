d9ccef6a5ab94f2c03af96288904bfc08d75d321
Rider Sargent <rider.sargent@gmail.com>
2020-08-13 13:06:22 -0400

    Completed refactor to private methods.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,72 +1,24 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      unless [ "Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"].include? item.name
-        return update_quality_normal(item)
-      end
-
-      if item.name == "Aged Brie"
+      case item.name
+      when "Aged Brie"
         return update_quality_brie(item)
-      end
-
-      if item.name == "Sulfuras, Hand of Ragnaros"
+      when "Sulfuras, Hand of Ragnaros"
         return update_quality_sulfuras(item)
-      end
-
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      when "Backstage passes to a TAFKAL80ETC concert"
         return update_quality_backstage_passes(item)
-      end
-
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        return update_quality_normal(item)
       end
     end
   end
+
+  private
 
   def update_quality_normal(item)
     item.sell_in -= 1
